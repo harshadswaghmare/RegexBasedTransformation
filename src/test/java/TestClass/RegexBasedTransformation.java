fcc94ef38365a0c59c391fbf3a5d800a2293bf51
Confluxsys <confluxsys@Mac.local>
2022-12-02 18:22:36 +0530

    first Commit

---
+++ src/test/java/TestClass/RegexBasedTransformation.java
@@ -0,0 +1,138 @@
+/*package TestClass;
+
+import java.util.regex.Pattern;
+
+class RegexBasedTransformation {
+    String forAccountRepresentative = "(?i)ACCOUNT REPRESENTATIVE";
+    String forAccountant1 = "((?i)ACCOUNTANT) I";
+
+    String forAccManager = "((?i)ACCOUNT MANAGER - OHS|Account Manager-OHS|ACCOUNT MANAGER|SR','ACCOUNT MANAGER SR|SR','ACCOUNT MANAGER, SR)";
+    String forAccountant2 = "((?i)ACCOUNTANT) II";
+    String forAcCordAMCO = "(?i)Accounting Coordinator-Amco";
+    String forAccPayClerk = "(?i)ACCOUNTS PAYABLE CLERK";
+    String forActuarialAnalyst = "(?i)ActuarialAnalyst";
+    String forActuary = "(?i)ACTUARY";
+    String forAdminSvcsCoordI = "(?i)ADMIN SVCS COORD I";
+    String forAdmin = "(?i)ADMIN";
+    String forAdministrativeclerk = "(?i)ADMINISTRATIVE CLERK";
+    String forAideHomeHealthString = "(?i)AIDE, HOME HEALTH";
+    String forAnalyst = "(?i)ANALYST";
+    String forAnalystData = "(?i)ANALYST,DATA";
+    String forAudiologist = "(?i)AUDIOLOGIST";
+    String forBsF = "(?i)BS&F Anl";
+    String forBsfConslt = "(?i)BS&F Conslt";
+    String behavioralMedSpecLcs = "Behavioral Medicine Spec LCSW|Behavioral Medicn Spec LCSW|Behavioral Medicn Spec\\(LCSW\\)";
+    String forBehavioralMedPsych = "(?i)BEHAVIORAL MED SPEC \\(PSYCH\\)|Behavioral Medicn Spec \\(Psych\\)";
+    String forLabAssistant1 = "((?i)LAB ASST) I";
+    String forLabAssistant2 = "((?i)LAB ASST) II";
+
+
+    //Constant For country
+
+    String forUS = "((?i)us|california)$";
+    String forIndia = "(?i)IND|india";
+    String forUK = "(?i)UK";
+    String forCameroon ="(?i)cameroon";
+    String forTrinidadTRO = "(?i)TrinidadTRO";
+    String forAfg = "(?i)AFG";
+    String forARG = "(?i)ARG";
+
+    String forNULL = "(?i)XXX|countr|country|111";
+
+    public String transform(String attr, String attrToBeReplaced) {
+        if (Pattern.matches(forAccountRepresentative, attr)) {
+            return (attr.replaceAll(attr, attrToBeReplaced));
+
+        } else if (Pattern.matches(forAccountant1, attr)) {
+            return (attr.replaceAll(attr, attrToBeReplaced));
+
+        } else if (Pattern.matches(forAccountant2, attr)) {
+            return (attr.replaceAll(attr, attrToBeReplaced));
+
+        } else if (Pattern.matches(forAccManager, attr.trim())) {
+            return (attr.replaceAll(attr, attrToBeReplaced));
+
+        } else if (Pattern.matches(forAcCordAMCO, attr.trim())) {
+            return (attr.replaceAll(attr, attrToBeReplaced));
+
+        } else if (Pattern.matches(forAccPayClerk, attr.trim())) {
+            return (attr.replaceAll(attr, attrToBeReplaced));
+
+        } else if (Pattern.matches(forActuarialAnalyst, attr.trim())) {
+            return (attr.replaceAll(attr, attrToBeReplaced));
+
+        } else if (Pattern.matches(forActuary, attr.trim())) {
+            return (attr.replaceAll(attr, attrToBeReplaced));
+
+        } else if (Pattern.matches(forAdminSvcsCoordI, attr.trim())) {
+            return (attr.replaceAll(attr, attrToBeReplaced));
+
+        } else if (Pattern.matches(forAdmin, attr)) {
+            return (attr.replaceAll(attr, attrToBeReplaced));
+
+        } else if (Pattern.matches(forAdministrativeclerk, attr.trim())) {
+            return (attr.replaceAll(attr, attrToBeReplaced));
+
+        } else if (Pattern.matches(forAideHomeHealthString, attr.trim())) {
+            return (attr.replaceAll(attr, attrToBeReplaced));
+
+        } else if (Pattern.matches(forAnalyst, attr.trim())) {
+            return (attr.replaceAll(attr, attrToBeReplaced));
+
+        } else if (Pattern.matches(forAnalystData, attr.trim())) {
+            return (attr.replaceAll(attr, attrToBeReplaced));
+
+        } else if (Pattern.matches(forAudiologist, attr.trim())) {
+            return (attr.replaceAll(attr, attrToBeReplaced));
+
+        } else if (Pattern.matches(forBsF, attr.trim())) {
+            return (attr.replaceAll(attr, attrToBeReplaced));
+
+        } else if (Pattern.matches(forBsfConslt, attr.trim())) {
+            return (attr.replaceAll(attr, attrToBeReplaced));
+
+        } else if (Pattern.matches(behavioralMedSpecLcs, attr)) {
+            return (attr.replaceAll(attr, attrToBeReplaced));
+
+        } else if (Pattern.matches(forBehavioralMedPsych, attr.trim())) {
+            return (attr.replaceAll(attr, attrToBeReplaced));
+
+        } else if (Pattern.matches(forLabAssistant1, attr.trim())) {
+            return (attr.replaceAll(attr, attrToBeReplaced));
+
+        } else if (Pattern.matches(forLabAssistant2, attr.trim())) {
+            return (attr.replaceAll(attr, attrToBeReplaced));
+         }
+
+        //for country
+        else if (Pattern.matches(forIndia, attr.trim())) {
+            return (attr.replaceAll(attr, attrToBeReplaced));
+
+        } else if (Pattern.matches(forUS, attr.trim())) {
+            return (attr.replaceAll(attr, attrToBeReplaced));
+
+        } else if (Pattern.matches(forUK, attr)) {
+            return (attr.replaceAll(attr, attrToBeReplaced));
+
+        } else if (Pattern.matches(forCameroon, attr.trim())) {
+            return (attr.replaceAll(attr, attrToBeReplaced));
+
+        } else if (Pattern.matches(forTrinidadTRO, attr.trim())) {
+            return (attr.replaceAll(attr, attrToBeReplaced));
+
+        } else if (Pattern.matches(forAfg, attr.trim())) {
+            return (attr.replaceAll(attr, attrToBeReplaced));
+        }
+        else if (Pattern.matches(forARG, attr.trim())) {
+            return (attr.replaceAll(attr, attrToBeReplaced));
+        }
+        else if (Pattern.matches(forNULL, attr.trim())) {
+            return (attr.replaceAll(attr, attrToBeReplaced));
+        }
+        return "Null";
+
+    }
+
+}*/
+
+
---
+++ src/test/java/org/example/CaseValue.java
@@ -0,0 +1,361 @@
+package org.example;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+public class CaseValue {
+    RegexBasedTransformation obj = new RegexBasedTransformation();
+
+    @org.junit.jupiter.api.Test
+    public void accountantRepresentative()
+    {
+        String result = obj.transform("ACCOUNT REPRESENTATIVE","Account Representative");
+        String result1= obj.transform("ACCOUNTANT","Account Representative");
+        System.out.println(result + "\n" +result1);
+        String expected = "Account Representative";
+        String Unexpected = "Harsh";
+        Assert.assertEquals(expected,result);
+        Assert.assertNotEquals(Unexpected,result1);
+    }
+
+
+
+
+        @Test
+        public void accountantI()
+        {
+
+            String result = obj.transform("ACCOUNTANT I" ,"Accountant I");
+            String result1 = obj.transform("Account I","Accountant I");
+            System.out.println(result);
+            System.out.println(result1);
+            String expected = "Accountant I";
+            String expected1 = "Null";
+            Assert.assertEquals(expected,result);
+            Assert.assertEquals(expected1,result1);
+
+        }
+
+
+    @org.junit.jupiter.api.Test
+    public void accountantII()
+    {
+
+        String result = obj.transform("ACCOUNTANT II" ,"Accountant II");
+        String result1 = obj.transform("Account II","Accountant II");
+        System.out.println(result +"\n" + result1);
+        String expected = "Accountant II";
+        String expected1 = "Null";
+        Assert.assertTrue(obj.transform("ACCOUNTANT II" ,"ACCOUNTANT II").contains("ACCOUNTANT II"));
+        Assert.assertEquals("Test passed ",expected1,result1);
+
+    }
+    @org.junit.jupiter.api.Test
+    public void accountingCoordAmco()
+    {
+        String result = obj.transform("Accounting Coordinator-Amco" ,"Accounting Coordinator - AMCO");
+        String result1 = obj.transform("ACC CORD AMCO","Accounting Coordinator - AMCO");
+        System.out.println(result + "\n" + result1);
+        String expected = "Accounting Coordinator - AMCO";
+        String expected1 = "Null";
+        Assert.assertEquals("Test Passed",expected,result);
+        Assert.assertEquals("Test passed ",expected1,result1);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void accPayableClerk()
+    {
+        String result = obj.transform("ACCOUNTS PAYABLE CLERK","Accounts Payable Clerk");
+        System.out.println(result);
+        String expected = "Accounts Payable Clerk";
+        Assert.assertEquals(expected,result);
+
+    }
+    @org.junit.jupiter.api.Test
+    public void actuarialAnalyst()
+    {
+        String result = obj.transform("ActuarialAnalyst","Actuarial Analyst");
+        System.out.println(result);
+        String expected = "Actuarial Analyst";
+        Assert.assertEquals(expected,result);
+
+    }
+
+    @org.junit.jupiter.api.Test
+    public void actuary() {
+        String result = obj.transform("ACTUARY", "Actuary");
+        System.out.println(result);
+        String expected = "Actuary";
+        Assert.assertEquals(expected, result);
+    }
+
+
+    @org.junit.jupiter.api.Test
+    public void adminSvcsCoord() {
+        String result = obj.transform("ADMIN SVCS COORD I", "Admin Svcs Coord I");
+        System.out.println(result);
+        String expected = "Admin Svcs Coord I";
+        Assert.assertEquals(expected, result);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void forAdmin()
+    {
+        String result = obj.transform("Admin","Admin");
+        System.out.println("After Transform attr is "+result);
+        String result1=obj.transform("akash","Admin");
+        String result2=obj.transform("ADMIN","Admin");
+        System.out.println(result + "\n" + result1 + "\n" + result2);
+        String expected  = "Admin";
+        String expected1 = "null";
+        String expected2 = "Admin";
+        Assert.assertEquals(expected ,result);
+        Assert.assertNotEquals(expected1,result1);
+        Assert.assertEquals(expected2,result2);
+    }
+
+
+    @org.junit.jupiter.api.Test
+    public void administrativeClerk() {
+        String result = obj.transform("ADMINISTRATIVE CLERK", "Administrative Clerk");
+        System.out.println(result);
+        String expected = "Administrative Clerk";
+        Assert.assertEquals(expected, result);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void aideHomeHealth() {
+        String result = obj.transform("AIDE, HOME HEALTH", "Aide, Home Health");
+        System.out.println(result);
+        String expected = "Aide, Home Health";
+        Assert.assertEquals(expected, result);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void analystOrAnalyst() {
+        String result = obj.transform("ANALYST ", "Analyst");
+        String result1 = obj.transform("analyst ", "Analyst");
+        System.out.println(result + "\n"+ result1);
+        String expected = "Analyst";
+        String expected1 = "Analyst";
+        Assert.assertEquals(expected, result);
+        Assert.assertEquals(expected1, result1);
+  }
+
+
+    @org.junit.jupiter.api.Test
+    public void analystData() {
+        String result = obj.transform("Analyst,Data","Analyst,Data");
+        System.out.println(result);
+        String expected = "Analyst,Data";
+        Assert.assertEquals(expected,result);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void audiologist() {
+        String result = obj.transform("AUDIOLOGIST","Audiologist");
+        System.out.println(result);
+        String expected = "Audiologist";
+        Assert.assertEquals(expected,result);
+    }
+    @org.junit.jupiter.api.Test
+    public void bsfConslt() {
+        String result = obj.transform("BS&F Conslt","BS&F Consultant");
+        System.out.println(result);
+        String expected = "BS&F Consultant";
+        Assert.assertEquals(expected,result);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void bsfAnl() {
+        String result = obj.transform("BS&F Conslt","BS&F Analyst");
+        String result1 = obj.transform("BS&F Consl","BS&F Analyst");
+        System.out.println(result +"\n"+ result1);
+        String expected = "BS&F Analyst";
+        String expected1 = "BS&F Analyst";
+        Assert.assertEquals(expected,result);
+        Assert.assertNotEquals(expected1,result1);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void labAssistantI() {
+        String result = obj.transform("Lab Asst I","Lab Assistant I");
+        String result1 = obj.transform("Lab ASST I ","Lab Assistant I");
+        String result2 = obj.transform("LAB ASST I","Lab Assistant I");
+        String result3 = obj.transform("LAB ASST ","Lab Assistant I");
+        System.out.println(result +"\n" + result1 + "\n" + result2 + "\n" + result3);
+        String expected = "Lab Assistant I";
+        String expected1 = "Lab Assistant I";
+        String expected2 = "Lab Assistant I";
+        String expected3 = "Lab assistant i";
+        Assert.assertEquals(expected,result);
+        Assert.assertEquals(expected1,result1);
+        Assert.assertEquals(expected2,result2);
+        Assert.assertNotEquals(expected3,result3);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void labAssistantII() {
+        String result = obj.transform("Lab Asst II","Lab Assistant II");
+        String result1 = obj.transform("Lab ASST II ","Lab Assistant II");
+        String result2 = obj.transform("LAB ASST II","Lab Assistant II");
+        String result3 = obj.transform("LAB ASST ","Lab Assistant II");
+        System.out.println(result +"\n" + result1 + "\n" + result2 + "\n" + result3 );
+        String expected = "Lab Assistant II";
+        String expected1 = "Lab Assistant II";
+        String expected2 = "Lab Assistant II";
+        String expected3 = "Lab assistant ii";
+        Assert.assertEquals(expected,result);
+        Assert.assertEquals(expected1,result1);
+        Assert.assertEquals(expected2,result2);
+        Assert.assertNotEquals(expected3,result3);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void show() {
+        String result = obj.transform("Behavioral Medicine Spec LCSW".trim(),"Behavioral Medicine Spec (LCSW)");
+        String result1 = obj.transform("Behavioral Medicn Spec LCSW".trim(),"Behavioral Medicine Spec (LCSW)");
+        String result2 = obj.transform("Behavioral Medicn Spec (LCSW)".trim(),"Behavioral Medicine Spec (LCSW)");
+        System.out.println(result +"\n" + result1 + "\n" + result2);
+        String expected2,expected1;
+        String expected =expected1=expected2 = "Behavioral Medicine Spec (LCSW)";
+        Assert.assertEquals(expected,result);
+        Assert.assertEquals(expected1,result1);
+        //Assert.assertEquals(expected2,result2);
+    }
+
+
+    @org.junit.jupiter.api.Test
+    public void accManager()
+    {
+        String result = obj.transform("ACCOUNT MANAGER - OHS".trim(),"Account Manager - OHS");
+        String result1 = obj.transform("Account Manager-OHS".trim(),"Account Manager - OHS");
+        String result2 = obj.transform("ACCOUNT MANAGER".trim(),"Account Manager - OHS");
+        String result3 = obj.transform("SR','ACCOUNT MANAGER, SR".trim(),"Account Manager - OHS");
+        String result4 = obj.transform("SR','ACCOUNT MANAGER SR".trim(),"Account Manager - OHS");
+        String result5= obj.transform("SR','ACCOUNT MANAGER ".trim(),"Account Manager - OHS");
+
+        System.out.println(result +"\n" + result1 + "\n" + result2 + "\n" + result3 + "\n" + result4 + "\n"+ result5);
+
+         String expected,expected1,expected2,expected3,expected4,expected5;
+        expected =expected1=expected2 =expected4=expected3 =expected5= "Account Manager - OHS";
+        Assert.assertEquals(expected,result);
+        Assert.assertEquals(expected1,result1);
+        Assert.assertEquals(expected2,result2);
+        Assert.assertEquals(expected3,result3);
+        Assert.assertEquals(expected4,result4);
+        Assert.assertNotEquals(expected5,result5);
+
+
+    }
+
+    //Test cases for country
+
+    @org.junit.jupiter.api.Test
+    public void forUS()
+    {
+        String result = obj.transform("US","United States of America");
+        String result1 = obj.transform("us","United States of America");
+        String result2 = obj.transform("CALIFORNIA","United States of America");
+        String result3 = obj.transform("California","United States of America");
+
+        System.out.println(result +"\n" + result1 + "\n" + result2 + "\n" + result3 );
+        String expected,expected1,expected2,expected3;
+        expected =expected1=expected2 =expected3= "United States of America";
+        Assert.assertEquals(expected,result);
+        Assert.assertEquals(expected1,result1);
+        Assert.assertEquals(expected2,result2);
+        Assert.assertEquals(expected3,result3);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void forIndia()
+    {
+        String result = obj.transform("ind","India");
+        String result1 = obj.transform("IND","India");
+        String result2 = obj.transform("INDIA","India");
+
+        System.out.println(result +"\n" + result1 + "\n" + result2 );
+        String expected,expected1,expected2;
+        expected =expected1=expected2 = "India";
+        Assert.assertEquals(expected,result);
+        Assert.assertEquals(expected1,result1);
+        Assert.assertEquals(expected2,result2);
+
+    }
+
+
+    @org.junit.jupiter.api.Test
+    public void afg()
+    {
+        String result = obj.transform("AFG","AFG");
+        System.out.println(result);
+        String expected = "AFG";
+        Assert.assertEquals(expected,result);
+
+    }
+
+    @org.junit.jupiter.api.Test
+    public void forCameroon()
+    {
+        String result = obj.transform("cameroon","Cameroon");
+        System.out.println(result);
+        String expected = "Cameroon";
+        Assert.assertEquals(expected,result);
+
+    }
+
+    @org.junit.jupiter.api.Test
+    public void forUk()
+    {
+        String result = obj.transform("uk","United Kingdom");
+        String result1 = obj.transform("uk","United Kingdom");
+        System.out.println(result + "\n" + result);
+        String expected = "United Kingdom";
+        String expected1= "United Kingdom";
+        Assert.assertEquals(expected,result);
+        Assert.assertEquals(expected1,result1);
+
+    }
+
+    @org.junit.jupiter.api.Test
+    public void forTrinidad()
+    {
+        String result = obj.transform("TrinidadTRO","Trinidad");
+        System.out.println(result);
+        String expected = "Trinidad";
+        Assert.assertEquals(expected,result);
+
+    }
+
+    @org.junit.jupiter.api.Test
+    public void forArgentina()
+    {
+        String result = obj.transform("ARG","Argentina");
+        System.out.println(result);
+        String expected = "Argentina";
+        Assert.assertEquals(expected,result);
+
+    }
+
+
+    @Test
+    public void forNull()
+    {
+        String result = obj.transform("XXX","NULL");
+        String result1 = obj.transform("countr","NULL");
+        String result2 = obj.transform("country","NULL");
+        String result3 = obj.transform("111","NULL");
+
+        System.out.println(result +"\n" + result1 + "\n" + result2 + "\n" + result3 );
+        String expected,expected1,expected2,expected3;
+        expected =expected1=expected2 =expected3= "NULL";
+        Assert.assertEquals(expected,result);
+        Assert.assertEquals(expected1,result1);
+        Assert.assertEquals(expected2,result2);
+        Assert.assertEquals(expected3,result3);
+    }
+
+
+
+}
